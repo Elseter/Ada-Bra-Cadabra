436faff411070f3bf791e9d8150b7893952f3f21
Elseter <rking3425@gmail.com>
2024-11-25 16:06:40 -0500

    Menu

--- Ada-Bra-Cadabra/src-tauri/src/lib.rs
+++ Ada-Bra-Cadabra/src-tauri/src/lib.rs
@@ -1,4 +1,10 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::{
+    menu::{AboutMetadata, MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    Manager,
+};
+use tauri::Emitter;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,7 +12,45 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+
+            let settings = MenuItemBuilder::new("Settings...")
+                .id("settings")
+                .accelerator("CmdOrCtrl+,")
+                .build(app)?;
+            
+            // my custom app submenu
+            let app_submenu = SubmenuBuilder::new(app, "App")
+                .about(Some(AboutMetadata {
+                    ..Default::default()
+                }))
+                .separator()
+                .item(&settings)
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .quit()
+                .build()?;
+            
+                let menu = MenuBuilder::new(app)
+                .items(&[
+                    &app_submenu,
+                ])
+                .build()?;
+
+                app.set_menu(menu);
+                app.on_menu_event(move |app, event| {
+                    if event.id() == settings.id(){
+                        let _event = app.emit("custom-event", "/settings");
+                    }
+                });
+                Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
